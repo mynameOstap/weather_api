66c9c7c725f7a43da4fb403171a9834c3efe6362
Ostap <ostduf2@gmail.com>
2024-12-11 16:29:05 +0200

    fix

--- weather_s/Service/GetWeather.cs
+++ weather_s/Service/GetWeather.cs
@@ -7,7 +7,7 @@
         {
             using (HttpClient client = new HttpClient())
             {
-                string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=23ccc7f23bbc8f2fdeb7d107e8077b5c&units=metric&lang=ua";
+                string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=API_KEY";
                
                 HttpResponseMessage response = await client.GetAsync(url);
                 string responseBody = await response.Content.ReadAsStringAsync();
